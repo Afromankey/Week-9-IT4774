a5e7c99f89f6427c4e5fde4354e7bb84fa7b2f9d
Afromankey <134185910+Afromankey@users.noreply.github.com>
2023-06-09 16:21:01 -0700

    Delete PhoneBook_NB directory

--- PhoneBook_NB/src/main/java/edu/capella/bsit/phonebook_nb/PhoneBook_NB.java
+++
@@ -1,8 +0,0 @@
-package edu.capella.bsit.phonebook_nb;
-
-public class PhoneBook_NB {
-
-    public static void main(String[] args) {
-        System.out.println("main() not implemented - Run tests instead.");
-    }
-}
